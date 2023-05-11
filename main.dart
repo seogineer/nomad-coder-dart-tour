12154081400294fb8bf7328ee778cb5a4241b91e
seogineer <dgseo8981@gmail.com>
2023-05-12 00:04:09 +0900

    Final Variables

--- main.dart
+++ main.dart
@@ -1,8 +1,4 @@
 void main() {
-  String? nickName = 'seogineer'; // ?를 붙이면 null이 될 수도 있다는 선언
-  nickName = null;
-  // if (nickName != null) {
-  //   nickName.isNotEmpty;
-  // }
-  nickName?.isNotEmpty;
+  final name = 'seogineer';
+  name = 'las';
 }
