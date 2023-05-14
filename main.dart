db5cad5a8769361f9a77758efcc56539ee38c408
seogineer <dgseo8981@gmail.com>
2023-05-14 15:08:37 +0900

    Named Parameters

--- main.dart
+++ main.dart
@@ -1,15 +1,29 @@
-// void sayHello(String name) {
-//   print("Hello $name nice to meet you!");
+// String sayHello(String name, int age, String country) {
+//   return "Hello $name, you are $age, and you come from $country";
 // }
 
-// String sayHello(String potato) {
-//   return "Hello $potato nice to meet you!";
+// String sayHello({
+//   String name = 'anonymous',
+//   int age = 19,
+//   String country = 'Korea',
+// }) {
+//   return "Hello $name, you are $age, and you come from $country";
 // }
 
-String sayHello(String potato) => "Hello $potato nice to meet you!";
-
-num plus(num a, num b) => a + b;
+String sayHello({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return "Hello $name, you are $age, and you come from $country";
+}
 
 void main() {
-  print(sayHello("seogineer"));
+  // print(sayHello('seogineer', 19, 'cuba'));
+
+  print(sayHello(
+    name: 'seogineer',
+    age: 19,
+    country: 'cuba',
+  ));
 }
