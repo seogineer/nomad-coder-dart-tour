ef720a7753f05faae1c3eb36a312ebadbace37a6
seogineer <dgseo8981@gmail.com>
2023-05-14 16:58:25 +0900

    Constructors

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 class Player {
-  final String name = 'seogineer';
-  int xp = 1500;
+  final String name;
+  int xp;
+
+  Player(this.name, this.xp);
 
   void sayHello() {
     print("Hello, My name is $name");
@@ -8,7 +10,8 @@ class Player {
 }
 
 void main() {
-  var player = Player();
-  print(player.name);
+  var player = Player('nico', 1500);
   player.sayHello();
+  var player2 = Player('seogineer', 2500);
+  player2.sayHello();
 }
