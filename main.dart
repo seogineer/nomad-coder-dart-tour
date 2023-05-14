514d4d51157d3ef0dac8bba3a2498060420587e4
seogineer <dgseo8981@gmail.com>
2023-05-14 15:22:27 +0900

    Optional Positional Parameters

--- main.dart
+++ main.dart
@@ -1,29 +1,6 @@
-// String sayHello(String name, int age, String country) {
-//   return "Hello $name, you are $age, and you come from $country";
-// }
-
-// String sayHello({
-//   String name = 'anonymous',
-//   int age = 19,
-//   String country = 'Korea',
-// }) {
-//   return "Hello $name, you are $age, and you come from $country";
-// }
-
-String sayHello({
-  required String name,
-  required int age,
-  required String country,
-}) {
-  return "Hello $name, you are $age, and you come from $country";
-}
+String sayHello(String name, int age, [String? country = 'cuba']) =>
+    "Hello $name, you are $age, and you come from $country";
 
 void main() {
-  // print(sayHello('seogineer', 19, 'cuba'));
-
-  print(sayHello(
-    name: 'seogineer',
-    age: 19,
-    country: 'cuba',
-  ));
+  print(sayHello('seogineer', 19));
 }
