20b96bcfbe71c7c14b2c37653de1b43cb875bfcb
seogineer <dgseo8981@gmail.com>
2023-05-14 23:35:40 +0900

    Enums

--- main.dart
+++ main.dart
@@ -1,7 +1,11 @@
+enum Team { red, blue }
+
+enum XPLevel { beginner, medium, pro }
+
 class Player {
   String name;
-  int xp;
-  String team;
+  XPLevel xp;
+  Team team;
 
   Player({
     required this.name,
@@ -15,10 +19,14 @@ class Player {
 }
 
 void main() {
-  var nico = Player(name: 'nico', xp: 1200, team: 'red');
+  var nico = Player(
+    name: 'nico',
+    xp: XPLevel.medium,
+    team: Team.red,
+  );
   var potato = nico
     ..name = 'las'
-    ..xp = 1200000
-    ..team = 'blue'
+    ..xp = XPLevel.pro
+    ..team = Team.blue
     ..sayHello();
 }
