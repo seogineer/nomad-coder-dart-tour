bbd50ac10ad2bf0fd52de96be75a8c984b833fa4
seogineer <dgseo8981@gmail.com>
2023-05-14 12:36:11 +0900

    basic data types

--- main.dart
+++ main.dart
@@ -1,6 +1,9 @@
 void main() {
-  // compile-time constant
-  // 컴파일 시점에 고정되어야 하는 상수값을 지정할 때 사용
-  // 앱이 실행되고 API를 통해 가져온 값을 갱신해야 하는 경우 final을 사용
-  const PI = 3.14;
+  String name = "seogineer";
+  bool alive = true;
+  int age = 12;
+  double money = 69.99;
+
+  num x = 12;
+  x = 1.1;
 }
