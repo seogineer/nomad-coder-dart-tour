8ad2f9317fa545af3f00ab8ed969e8dd5ee3e5e9
seogineer <dgseo8981@gmail.com>
2023-05-15 00:08:44 +0900

    Inheritance

--- main.dart
+++ main.dart
@@ -1,48 +1,32 @@
-abstract class Human {
-  void walk();
-}
-
-enum Team { red, blue }
-
-enum XPLevel { beginner, medium, pro }
-
-class Player extends Human {
-  String name;
-  XPLevel xp;
-  Team team;
-
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-  });
-
+class Human {
+  final String name;
+  Human({required this.name});
   void sayHello() {
     print("Hi my name is $name");
   }
-
-  @override
-  void walk() {
-    print('im walking');
-  }
 }
 
-class Coach extends Human {
+enum Team { blue, red }
+
+class Player extends Human {
+  final Team team;
+
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name: name);
+
   @override
-  void walk() {
-    print('the coach is walking');
+  void sayHello() {
+    super.sayHello();
+    print('and I play for ${team}');
   }
 }
 
 void main() {
-  var nico = Player(
-    name: 'nico',
-    xp: XPLevel.medium,
+  var player = Player(
     team: Team.red,
+    name: 'seogineer',
   );
-  var potato = nico
-    ..name = 'las'
-    ..xp = XPLevel.pro
-    ..team = Team.blue
-    ..sayHello();
+  player.sayHello();
 }
