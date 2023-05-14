06df3edcadb208ec5fcca498273be4106a568091
seogineer <dgseo8981@gmail.com>
2023-05-14 23:20:09 +0900

    Named Constructors 2

--- main.dart
+++ main.dart
@@ -1,28 +1,12 @@
 class Player {
   final String name;
-  int xp, age;
+  int xp;
   String team;
 
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age,
-  });
-
-  Player.createBluePlayer({
-    required String name,
-    required int age,
-  })  : this.age = age,
-        this.name = name,
-        this.team = 'blue',
-        this.xp = 0;
-
-  Player.createRedPlayer(String name, int age)
-      : this.age = age,
-        this.name = name,
-        this.team = 'red',
-        this.xp = 0;
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
 
   void sayHello() {
     print("Hi my name is $name");
@@ -30,9 +14,26 @@ class Player {
 }
 
 void main() {
-  var player = Player.createBluePlayer(
-    name: "nico",
-    age: 21,
-  );
-  var player2 = Player.createRedPlayer("seogineer", 30);
+  var apiDate = [
+    {
+      "name": "nico",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lynn",
+      "team": "blue",
+      "xp": 0,
+    },
+    {
+      "name": "seogineer",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+
+  apiDate.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
